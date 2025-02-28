2c404476988a9ae6117bc22f2e50d46bd70d58d0
pabcascom <pabcascom@alum.us.es>
2025-02-28 21:05:54 +0100

    plantilla terminada

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Helmet from "react-helmet";
 import "../static/resources/css/Home.css"
 
