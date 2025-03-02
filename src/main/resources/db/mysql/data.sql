093fc426128d413f18ef0156bc4e6581ee10f45a
Andrés Pizzano <andpizcer@alum.us.es>
2025-03-02 18:38:31 +0100

    feat: add sql tables of entities.
    Creating new tables based on UML design #47

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO users (id, username, email, first_name, last_name, telephone, password) VALUES 
+INSERT IGNORE INTO users (userId, username, email, first_name, last_name, telephone, password) VALUES 
 (1, 'alice123', 'alice@example.com', 'Alice', 'Johnson', '123456789', '$2a$10$gyqEuh5bpJVhwuN44YwkjeaR6/1u2KqFTGhjYxWtH27Dmka569AmK'),
 (2, 'bob456', 'bob@example.com', 'Bob', 'Smith', '987654321', '$2a$10$gyqEuh5bpJVhwuN44YwkjeaR6/1u2KqFTGhjYxWtH27Dmka569AmK'),
 (3, 'charlie789', 'charlie@example.com', 'Charlie', 'Brown', '567123890', '$2a$10$gyqEuh5bpJVhwuN44YwkjeaR6/1u2KqFTGhjYxWtH27Dmka569AmK'),
