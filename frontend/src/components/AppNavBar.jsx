b0578e4d9aec918f0cbddd99239c53e92839c7d8
Ignacio Blanquero <ignacio.blanquero@gmail.com>
2025-03-04 10:32:37 +0100

    feat: improvements in the AppNavbar
    Added links to user and messages icons for better navigation #67

--- frontend/src/components/AppNavBar.jsx
+++ frontend/src/components/AppNavBar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../components/AppNavBar.css";
 import logo from "../static/resources/images/logo-eventbride.png";
+import carta from "../Static/resources/images/carta.png";
+import usuario from "../Static/resources/images/user.png";
 
 function Navbar() {
   return (
@@ -11,12 +13,21 @@ function Navbar() {
       </div>
       <ul className="navbar-list">
         <li><a href="/">Inicio</a></li>
-        <li><a href="/perfil">Perfil</a></li>
         <li><a href="/miseventos">Mis eventos</a></li>
         <li><a href="/lugares">Lugares</a></li>
         <li><a href="/proveedores">Proveedores</a></li>
         <li><a href="/invitaciones">Invitaciones</a></li>
       </ul>
+      <div className="navbar-card">
+        <a href="/mensajes">
+          <img src={carta} alt="Carta" className="carta" />
+        </a>
+      </div>
+      <div className="navbar-user">
+        <a href="/perfil">
+          <img src={usuario} alt="Usuario" className="usuario" />
+        </a>
+      </div>
     </nav>
   );
 }
