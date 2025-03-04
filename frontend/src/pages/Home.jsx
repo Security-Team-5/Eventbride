ad18bd902504ddd344aabc2e6cefa8c482173c8e
María Carrera <marcartal1@alum.us.es>
2025-03-04 20:54:04 +0100

    fix: remove useless navbar component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
-import Helmet from "react-helmet";
 import "../static/resources/css/Home.css";
 import "../components/AppNavBar.css";
-import Navbar from "../components/AppNavBar";
 
 // eslint-disable-next-line react/prop-types
 function Home({ user }) {
@@ -57,7 +55,6 @@ function Home({ user }) {
 
   return (
     <div className="home-container">
-      <Navbar /> {/* Se agrega el Navbar en la parte superior */}
       <main className="home-main">
         <h2 className="welcome-text">
           Hola usuario: {currentUser?.username || "Desconocido"}
