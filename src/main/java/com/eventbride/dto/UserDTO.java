487cd9eb3c169ea4dac38e5e99c4e58d8c21caa8
yato03 <miguelhs3523@gmail.com>
2025-03-06 11:51:34 +0100

    fix: Register fixed
    No comments needed #50

---
+++ src/main/java/com/eventbride/dto/UserDTO.java
@@ -0,0 +1,47 @@
+package com.eventbride.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.eventbride.user.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private String lastname;
+    private String firstname;
+    private String username;
+    private String email;
+    private Integer telephone;
+    private String dni;
+    private String profilePicture;
+    private String role;
+    private List<EventDTO> events;
+
+    // Constructor que toma la entidad User y la convierte a DTO
+    public UserDTO(User user) {
+        this.lastname = user.getLastName();
+        this.firstname = user.getFirstName();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.telephone = user.getTelephone();
+        this.dni = user.getDni();
+        this.profilePicture = user.getProfilePicture();
+        this.role = user.getRole();
+        if(user.getEvents() != null){
+            this.events = user.getEvents().stream()
+                    .map(EventDTO::new)  // Convertir la lista de eventos a DTOs
+                    .collect(Collectors.toList());
+        }
+    }
+
+    // Método estático para convertir una lista de usuarios en una lista de DTOs
+    public static List<UserDTO> fromEntities(List<User> users) {
+        return users.stream()
+                .map(UserDTO::new)
+                .collect(Collectors.toList());
+    }
+}
