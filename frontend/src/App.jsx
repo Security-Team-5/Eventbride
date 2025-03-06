941a428b1f445a5ed3a5e0347cfd55af8d6192a3
yato03 <miguelhs3523@gmail.com>
2025-03-06 18:54:41 +0100

    feat: Venue list with VenueDTO
    No comments needed #59

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -14,7 +13,7 @@ function App() {
   if (loading) {
     return <div>Loading...</div>; // Muestra algo mientras se carga el usuario
   }
-
+  
   return (
     <div className="app-container"> 
       <Router>
