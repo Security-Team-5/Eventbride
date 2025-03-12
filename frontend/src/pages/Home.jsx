d689d3203b05f04a0fa1bede5be9f0167a9d63b8
Adrian <adriancabellomartin2003@gmail.com>
2025-03-13 00:03:54 +0100

    fix:Home updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,22 +10,27 @@ import { useCurrentUser } from "../hooks/useCurrentUser";
 function Home({ user }) {
   return (
     <main className="contenedorPrincipal">
-      {/* Banner de ancho completo */}
-      <div className="bannerCompleto">
+      <div className="bannerCompleto" >
         <img src="https://t4.ftcdn.net/jpg/04/10/98/29/360_F_410982967_TiQ653hlfXQrI4vuLpEBwMpLOYR013fK.jpg" alt="Banner principal" className="imagenBanner" />
         <div className="overlayBanner">
           <div className="contenidoBanner">
-            <img src={logo} alt="Eventbride Logo" className="logo-home" />
+            <img src={logo} alt="Eventbride Logo" className="logo-home" style={{ marginTop: "20%"}} />
           </div>
         </div>
       </div>
 
-      {/* Sección de bienvenida y quiénes somos */}
-      <section className="seccionBienvenida">
+      <section className="seccionValores">
         <div className="contenedor">
           <div className="encabezadoCentrado">
+            <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet"></link>
             <h2 className="tituloSeccion">Bienvenidos</h2>
             <div className="separador"></div>
+            <p className="textoDescriptivo">
+              Eventbride 
+              <br></br>
+              Eventos que brillan, recuerdos que perduran.              
+            </p>
+            <div className="separador" style={{ marginTop: "3%"}}></div>
             <p className="textoDescriptivo">
               ¡Bienvenido a Eventbride, donde cada momento especial se convierte en una celebración única y sin preocupaciones!
             </p>
@@ -38,6 +43,7 @@ function Home({ user }) {
                 <h3 className="tituloTarjeta">Nuestra Misión</h3>
                 <p className="textoTarjeta">
                   Nuestra misión es ayudarte a planificar y organizar esos momentos tan especiales en la vida: comuniones, bautizos y bodas.
+                  <br></br>
                   Con nuestras herramientas fáciles de usar, podrás gestionar todos los detalles importantes, desde las fechas hasta los proveedores,
                   y asegurarte de que tu celebración sea perfecta.
                 </p>
@@ -46,11 +52,17 @@ function Home({ user }) {
                 <h3 className="tituloTarjeta">Nuestra Visión</h3>
                 <p className="textoTarjeta">
                   En Eventbride, nuestra misión es hacer que la organización sea fácil, eficiente y libre de estrés.
+                  <br></br>
                   Nos comprometemos a ofrecer una plataforma intuitiva que permita a los usuarios gestionar cada detalle con confianza y tranquilidad.
+                  <br></br>
                   Queremos que disfrutes del proceso de planificación, asegurándonos de que cada evento sea memorable y único, sin preocuparte por los pequeños detalles.
                 </p>
               </div>
             </div>
+          </div>
+        </div>
+        <div className="contenedor">
+        <div className="seccionQuienesSomos">
             <h2 className="tituloSeccion">Crear evento</h2>
             <div className="contenedorColumnas">
               <div className="tarjeta">
@@ -58,7 +70,7 @@ function Home({ user }) {
                 <p className="textoTarjeta">
                   Crea un evento de cero a tu gusto.
                 </p>
-                <a href="/create-events" style={{ display: 'flex', margin: '0 auto', marginTop: '46px', border: '1px solid rgb(0, 0, 0)', backgroundColor: 'rgba(247, 217, 227, 0.96)', borderRadius: '10px', padding: '10px', height: '7%', width: '25%', textAlign: 'center', color: "black", flexDirection: "column", justifyContent: "center" }} >
+                <a href="/create-events"  style={{ display: 'flex', margin: '0 auto', marginTop: '46px', border: '1px solid rgb(0, 0, 0)', backgroundColor: 'rgba(247, 217, 227, 0.96)', borderRadius: '10px', padding: '10px',height: '7%', width: '25%', textAlign: 'center', color: "black", flexDirection:"column", justifyContent:"center"}} >
                   Crear evento
                 </a>
 
@@ -68,32 +80,28 @@ function Home({ user }) {
                 <p className="textoTarjeta">
                   Te ofrecemos un cuestionario para ayudarte a que puedas encontrar el evento perfecto para ti.
                 </p>
-                <a style={{ display: 'flex', margin: '0 auto', marginTop: '20px', border: '1px solid rgb(0, 0, 0)', backgroundColor: 'rgba(247, 217, 227, 0.96)', borderRadius: '10px', padding: '10px', height: '7%', width: '25%', textAlign: 'center', color: "black", flexDirection: "column", justifyContent: "center" }} >
+                <a style={{ display: 'flex', margin: '0 auto', marginTop: '20px', border: '1px solid rgb(0, 0, 0)', backgroundColor: 'rgba(247, 217, 227, 0.96)', borderRadius: '10px', padding: '10px',height: '7%', width: '25%', textAlign: 'center', color: "black", flexDirection:"column", justifyContent:"center"}} >
                   Cuestionario
                 </a>
-              </div>
+                </div>
             </div>
           </div>
         </div>
-      </section>
-
-      {/* Sección de valores o servicios */}
-      <section className="seccionValores">
         <div className="contenedor">
           <h2 className="tituloSeccion">Packs destacados</h2>
           <div className="contenedorValores">
             {[
               {
                 title: "Bautizos",
-                description: "Nos comprometemos a ofrecer productos y servicios de la más alta calidad.",
+                description: "Un día lleno de amor y emoción para dar la bienvenida a una nueva vida. Celebra con nosotros.",
               },
               {
                 title: "Bodas",
-                description: "Actuamos con honestidad y transparencia en todas nuestras operaciones.",
+                description: "El día más especial merece una celebración inolvidable. Creamos el ambiente perfecto para tu gran momento.",
               },
               {
                 title: "Comuniones",
-                description: "Buscamos constantemente nuevas formas de mejorar y superar expectativas.",
+                description: "Un evento único para un día inolvidable. Hacemos que la primera comunión sea mágica.",
               },
             ].map((valor, index) => (
               <div key={index} className="tarjetaValor">
