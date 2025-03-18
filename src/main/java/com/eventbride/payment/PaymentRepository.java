326fc20997c4544ce636c3b1e767b9a4aad5e86c
Francisco Avilés Carrera <fraavicar1@alum.us.es>
2025-03-18 23:03:35 +0100

    feat: add PaymentController, PaymentRepository and PaymentService
    No description needed #60

--- src/main/java/com/eventbride/payment/PaymentRepository.java
+++ src/main/java/com/eventbride/payment/PaymentRepository.java
@@ -1,9 +1,9 @@
 package com.eventbride.payment;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentRepository extends CrudRepository<Payment, Integer> {
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
     
 }
---
+++ src/main/java/com/eventbride/payment/PaymentService.java
@@ -0,0 +1,59 @@
+package com.eventbride.payment;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eventbride.event_properties.EventProperties;
+import com.eventbride.event_properties.EventPropertiesRepository;
+import com.eventbride.event_properties.EventProperties.Status;
+import com.eventbride.payment.Payment.PaymentType;
+import com.eventbride.user.User;
+import com.eventbride.user.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class PaymentService {
+    private PaymentRepository paymentRepository;
+    private EventPropertiesRepository eventPropertiesRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public PaymentService(PaymentRepository paymentRepository, EventPropertiesRepository eventPropertiesRepository,
+            UserRepository userRepository) {
+        this.paymentRepository = paymentRepository;
+        this.eventPropertiesRepository = eventPropertiesRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public Payment payDeposit(Integer eventPropertiesId, Integer userId) {
+        EventProperties e = eventPropertiesRepository.findById(eventPropertiesId).orElse(null);
+        Payment p;
+
+        if (e != null) {
+            e.setStatus(Status.DEPOSIT_PAID);
+            eventPropertiesRepository.save(e);
+
+            p = new Payment();
+
+            p.setAmount(e.getDepositAmount());
+            p.setDateTime(LocalDateTime.now());
+            p.setPaymentType(PaymentType.DEPOSIT);
+            p.setEventProperties(e);
+            User user = userRepository.findById(userId).orElse(null);
+
+            if (user == null)
+                return null;
+
+            p.setUser(user);
+
+            paymentRepository.save(p);
+        } else {
+            p = null;
+        }
+        return p;
+    }
+}
