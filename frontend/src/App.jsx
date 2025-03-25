7a219d17a94751b18065b74bc89f2424ac388cf9
pabcascom <pabcascom@alum.us.es>
2025-03-26 00:18:53 +0100

    fix: routes fixed
    No comments needed #270

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,6 +21,8 @@ import AdminUsers from "./pages/admin/AdminUsers"
 import InformationService from "./pages/InformationService"
 import Payment from "./pages/Payment"
 import Footer from "./components/Footer"
+import Profile from "./pages/EditProfile"
+import EditPlanProfile from "./pages/EditPlanProfile";
 
 function App() {
   const { currentUser, loading, setCurrentUser } = useCurrentUser(null)
@@ -51,6 +53,8 @@ function App() {
             <Route path="/create-events" element={<CreateEvents />} />
             <Route path="/event/:id" element={<EventDetails />} />
             <Route path="/payment/:id" element={<Payment />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/plan" element={<EditPlanProfile />} />
             <Route path="/other-services/information/:id" element={<InformationService />} />
             {currentUser && currentUser.role == "ADMIN" && (
               <>
